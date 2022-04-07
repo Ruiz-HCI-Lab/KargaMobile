c1d2d3fd367f52f355879cfa6915cb3edf144975
Alex Barquero <a.barquero@ruizlab.org>
2022-04-07 11:13:59 -0400

    1. Adding the placeholder RecycleList to display a gene list in the Results activity.

--- app/src/main/java/org/ruizlab/phoni/kargamobile/Mapper.java
+++ app/src/main/java/org/ruizlab/phoni/kargamobile/Mapper.java
@@ -7,7 +7,6 @@
 package org.ruizlab.phoni.kargamobile;
 
 import android.content.Context;
-import android.content.res.AssetManager;
 import android.net.Uri;
 
 import androidx.annotation.*;
@@ -16,7 +15,6 @@ import androidx.work.*;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
--- app/src/main/java/org/ruizlab/phoni/kargamobile/Results.java
+++ app/src/main/java/org/ruizlab/phoni/kargamobile/Results.java
@@ -6,10 +6,19 @@
 // -----------------------------------------------------------------------
 package org.ruizlab.phoni.kargamobile;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
-public class Results extends AppCompatActivity {
+public class Results extends AppCompatActivity implements GeneListAdapter.ItemClickListener{
+
+    GeneListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +26,25 @@ public class Results extends AppCompatActivity {
         setContentView(R.layout.activity_results);
 
         getSupportActionBar().hide();
+
+        // data to populate the RecyclerView with
+        ArrayList<String> geneList = new ArrayList<>();
+        geneList.add(">MEG_1000|Drugs|Aminoglycosides|Aminoglycoside_O-nucleotidyltransferases|ANT6");
+        geneList.add(">MEG_1002|Drugs|Aminoglycosides|Aminoglycoside_O-nucleotidyltransferases|ANT6");
+        geneList.add(">MEG_1003|Drugs|Aminoglycosides|Aminoglycoside_O-nucleotidyltransferases|ANT9");
+        geneList.add(">MEG_2419|Drugs|betalactams|Class_A_betalactamases|CTX");
+        geneList.add(">MEG_6447|Drugs|betalactams|Class_A_betalactamases|SHV");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.rvGeneList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new GeneListAdapter(this, geneList);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 }
